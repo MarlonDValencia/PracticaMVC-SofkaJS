14a109723b81dfe724559c12a4df45e887d2b411
MarlonDValencia <marlondvalencia@gmail.com>
2021-11-19 12:47:34 -0500

    Solucionado problema canvas

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-(() => {
-  class Board {
-    constructor(width, height) {
+(() => { 
+  self.Board = function(width, height){
       this.width = width;
       this.height = height;
       this.playing = false;
       this.game_over = false;
       this.bars = [];
+      this.ball = null
     }
-  }
-
   self.Board.prototype = {
     get elements(){
       var elements = this.bars
@@ -18,22 +16,20 @@
   }
 })();
 
-(()=>{
-  class BoardView {
-    constructor(canvas, board) {
+(() => {
+  self.BoardView = function(canvas, board){
       this.canvas = canvas;
       this.canvas.width = board.width;
       this.canvas.height = board.height;
       this.board = board;
       this.ctx = canvas.getContext("2d");
     }
-  }
 })()
 
 window.addEventListener("load",main);
 
 function main(){
   var board = new Board(800,400);
-  var canvas = document.getElementById("canvas",board);
-  var board_view = new BoardView(); 
+  var canvas = document.getElementById('canvas');
+  var board_view = new BoardView(canvas,board);
 }
