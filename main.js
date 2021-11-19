c07f7eac05e5e81efe1db83ea1e42d48d1728f13
MarlonDValencia <marlondvalencia@gmail.com>
2021-11-19 16:07:43 -0500

    Pelota del ping pong creada

--- main.js
+++ main.js
@@ -10,10 +10,24 @@
     }
   self.Board.prototype = {
     get elements(){
-      var elements = this.bars;
+      var elements = this.bars.map(function(bar){return bar});
       elements.push(this.ball);
       return elements;
     }
+  }
+})();
+
+(function(){
+  self.Ball = function(x,y,radius,board){
+    this.x =x;
+    this.y=y;
+    this.radius = radius;
+    this.speed_y = 0;
+    this.peed_x = 3;
+    this.board = board;
+
+    board.ball = this;
+    this.kind = "circle";
   }
 })();
 
@@ -52,43 +66,70 @@ self.Bar.prototype = {
     }
 
     self.BoardView.prototype = {
+      clean: function(){
+        this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+      },
       draw: function(){
         for(var i = this.board.elements.length-1; i >= 0; i--){
           var el = this.board.elements[i];
 
           draw(this.ctx,el)
         }
+      },
+      play: function(){
+        this.clean()
+        this.draw()
       }
     }
 
     function draw(ctx,element){
-      if(element !== null && element.hasOwnProperty("kind")){
         switch(element.kind){
           case "rectangle":
             ctx.fillRect(element.x, element.y, element.width, element.height)
-            break
+            break;
+          case "circle":
+            ctx.beginPath();
+            ctx.arc(element.x, element.y, element.radius,0,7);
+            ctx.fill();
+            ctx.closePath();
+            break;
         }
       }
-    }
 })();
 var board = new Board(800,400);
 var bar = new Bar(20,100,40,100,board);
-var bar = new Bar(700,100,40,100,board);
+var bar_2 = new Bar(700,100,40,100,board);
 var canvas = document.getElementById('canvas');
-var board_view = new BoardView(canvas,board); 
+var board_view = new BoardView(canvas,board);
+var ball = new Ball(350, 100, 10,board)
+
+window.requestAnimationFrame(controller);
+//Función para mover las barras con las flechas del teclado
 document.addEventListener("keydown", (ev)=>{
-  if(ev.keyCode == 38){
+  if(ev.keyCode == 87){
     bar.up();
+    //KeyUp
   }
-  else if(ev.keyCode == 40){
+  else if(ev.keyCode == 83){
     bar.down();
+    //KeyDown
+  }
+  if(ev.keyCode == 38){
+    bar_2.up();
+    //W
+  }
+
+  else if(ev.keyCode == 40){
+    bar_2.down();
+    //S
   }
 
   console.log(""+bar);
 })
 
-window.addEventListener("load",main);
+window.addEventListener("load",controller);
 //Controlador
-function main(){
-  board_view.draw()
+function controller(){
+  board_view.play();
+  window.requestAnimationFrame(controller);
 }
